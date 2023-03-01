03dc945d99f70ab880107736ba52bbd4a2cf37a3
Serigne78 <114218586+Serigne78@users.noreply.github.com>
2023-03-01 18:04:30 +0100

    ajout de la fonction right_dir et la fontion left_dir qui permettrons à la foumi de trouner à droite ou à gauche

--- fourmi_L1/main.py
+++ fourmi_L1/main.py
@@ -20,4 +20,23 @@ droit = 1
 bas = 2
 gauche = 3
 
+def right_dir(direction):
+    # Calcule la direction à droite de la direction donnée.
+    # La direction est un entier compris entre 0 et 3.
+    # La fonction renvoie un entier compris entre 0 et 3.
+    return (direction + 1) % 4
+
+
+def left_dir(direction):
+    # Calcule la direction à gauche de la direction donnée.
+    # La direction est un entier compris entre 0 et 3.
+    # La fonction renvoie un entier compris entre 0 et 3.
+    # Le résultat est calculé en soustrayant 1 de la direction
+    # donnée, en prenant le modulo 4 pour s'assurer que le
+    # résultat reste compris entre 0 et 3.
+    return (direction - 1) % 4
+
+
+
+
 
